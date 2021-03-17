7ba8197192d022aea10638ebc4201f1aa3db3b1f
John Paul <28johnpaulsim28@gmail.com>
2021-03-17 23:51:37 +0800

    design

--- resources/views/livewire/products-table.blade.php
+++ resources/views/livewire/products-table.blade.php
@@ -7,7 +7,7 @@
                 {{ session('message') }}
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
-                  </button>
+                </button>
             </div>
             @endif
         </div>
