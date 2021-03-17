32e7856888ac85462d879a58f1dfd515c820bc69
John Paul <28johnpaulsim28@gmail.com>
2021-03-17 23:49:45 +0800

    add button

--- resources/views/livewire/products-table.blade.php
+++ resources/views/livewire/products-table.blade.php
@@ -5,6 +5,9 @@
             @if (session()->has('message'))
             <div class="alert alert-success">
                 {{ session('message') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
             </div>
             @endif
         </div>
