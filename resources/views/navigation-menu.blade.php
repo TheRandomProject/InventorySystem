a28df27e1e08d5efdfcfb9d7b17a20660350a934
John Paul Sim <johnpaul.sim@hmrphils.com>
2021-04-12 18:35:23 +0800

    fix miss typo

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -28,7 +28,7 @@
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('products') }}" :active="request()->routeIs('products')">
-                        {{ __('INventory') }}
+                        {{ __('Inventory') }}
                     </x-jet-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
